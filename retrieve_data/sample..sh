bbfca619605b56be84c5a8c590752e25675efbd2
Mithun Gowda <114331144+mithun-gowda@users.noreply.github.com>
2024-03-04 07:16:14 +0000

    adding sample.sh file

---
+++ retrieve_data/sample..sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo Welcome to Console
+
+read name
+
+echo Hi, $name
+
+read -p 'Username:' uservar
+read -sp 'Password:' passvar
+
+echo Thankyou $uservar we now have your user details
