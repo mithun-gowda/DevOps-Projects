ff668da2fe0136b17f6e3b56566b811ff5b8c2e9
Ubuntu <ubuntu@ip-172-31-17-55.ec2.internal>
2024-03-05 07:25:40 +0000

    Adding cpu_monitor.sh file

---
+++ retrieve_data/cpu_monitor.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+##############
+# Author: Mithun Gowda
+# Date: 05-03-2024
+# 
+#
+# This script monitors the CPU usage and logs the information to a file
+#
+##############
+
+set -x
+set -o
+
+# Log file path
+LOG_FILE="/var/log/cpu_monitor.log"
+
+# Function to get current CPU usage
+get_cpu_usage() {
+    # Using 'top' command to get CPU usage
+    CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}')
+    echo "$CPU_USAGE"
+}
+
+# Main function
+main() {
+    # Get current timestamp
+    TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+
+    # Get CPU usage
+    CPU_USAGE=$(get_cpu_usage)
+
+    # Log CPU usage to file
+    echo "$TIMESTAMP - CPU Usage: $CPU_USAGE%" >> "$LOG_FILE"
+}
+
+# Run main function
+main
+
